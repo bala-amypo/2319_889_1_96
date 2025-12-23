f64c492c248de0bae5506189d6f0142449b3cb95
bala-amypo <bala@amypo.in>
2025-12-23 05:25:33 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/security/CustomUserDetailService.java
@@ -0,0 +1,26 @@
+package com.example.demo.security;
+
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        // Demo in-memory user (replace with DB later)
+        if (!"admin".equals(username)) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return new User(
+                "admin",
+                "{noop}password", // no-encoding for demo
+                Collections.singleton(() -> "ROLE_ADMIN")
+        );
+    }
+}
