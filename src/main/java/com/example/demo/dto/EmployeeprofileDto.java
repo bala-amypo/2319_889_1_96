5af3790a039c6939fc1b3c0c50cc7f3551bfdb3e
bala-amypo <bala@amypo.in>
2025-12-29 05:13:41 +0000

    User commit

--- src/main/java/com/example/demo/dto/EmployeeprofileDto.java
+++ src/main/java/com/example/demo/dto/EmployeeprofileDto.java
@@ -1,39 +1,24 @@
 package com.example.demo.dto;
 
 public class EmployeeProfileDto {
+
     private Long id;
-    private String employeeId;
-    private String fullName;
-    private String email;
-    private String teamName;
-    private String role;
+    private String name;
 
-    public EmployeeProfileDto() {}
-
-    public EmployeeProfileDto(Long id, String employeeId, String fullName, String email, String teamName, String role) {
-        this.id = id;
-        this.employeeId = employeeId;
-        this.fullName = fullName;
-        this.email = email;
-        this.teamName = teamName;
-        this.role = role;
+    // getters & setters
+    public Long getId() {
+        return id;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getEmployeeId() { return employeeId; }
-    public void setEmployeeId(String employeeId) { this.employeeId = employeeId; }
+    public String getName() {
+        return name;
+    }
 
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public String getTeamName() { return teamName; }
-    public void setTeamName(String teamName) { this.teamName = teamName; }
-
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
